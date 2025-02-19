89e129f23eb41527ecacb4426b990c7befb13b75
David Smith <davidssmith93@gmail.com>
2025-02-19 15:21:48 +0000

    adds initial implementation

--- Search/Flights/FlightLookup.cs
+++ Search/Flights/FlightLookup.cs
@@ -1,22 +1,55 @@
-using System;
+using System.Security.AccessControl;
+using Newtonsoft.Json;
 
 namespace Search.Flights;
 
 public class FlightLookup
 {
 
-    private static Lazy<FlightLookup> Instance = new(() => new());
+    private static readonly Lazy<FlightLookup> Instance = new(() => new());
+
+    private List<Flight> Flights;
 
     public static FlightLookup GetInstance() {
         return Instance.Value;
     }
 
-    public List<FlightResult> Search(String from, String to, DateOnly date) {
-        return [];
+    public FlightLookup() {
+        Load();
     }
 
+    private void Load()
+    {
+        try
+        {
+            using StreamReader reader = new("flights.json");
+
+            string content = reader.ReadToEnd();
+
+            List<Flight>? flights = JsonConvert.DeserializeObject<List<Flight>>(content) ?? throw new Exception();
+            this.Flights = flights;
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+            
+            throw new Exception();
+        }
+    }
+
+    public List<Flight> Search(string from, string to, DateOnly date) {
+        return Flights.FindAll(flight => flight.From == from && flight.To == to && flight.DepartureDate == date);
+    }
 }
 
-public record FlightResult()
+public record Flight(
+    [JsonPropertyAttribute("id")] int Id,
+    [JsonPropertyAttribute("airline")] string Airline,
+    [JsonPropertyAttribute("from")] string From,
+    [JsonPropertyAttribute("to")] string To,
+    [JsonPropertyAttribute("price")] decimal Price,
+    [JsonPropertyAttribute("departure_date")] DateOnly DepartureDate
+)
 {
 }
--- SearchTests/Flights/FlightLookupTests.cs
+++ SearchTests/Flights/FlightLookupTests.cs
@@ -1,6 +1,3 @@
-using System.Runtime.ConstrainedExecution;
-using Microsoft.VisualBasic;
-using NUnit.Framework;
 using Search.Flights;
 
 namespace SearchTests.Flights;
@@ -17,15 +14,26 @@ public class FlightLookupTests
     }
 
     [Test]
-    public void FlightDataLoadedCorrectly()
+    public void ExpectedFlightReturned_WhenFromMANToAGPOnJulyFirst()
     {
         string from = "MAN";
         string to = "AGP";
         DateOnly date = new(2023, 7, 1);
 
-        List<FlightResult> results = subject.Search(from, to, date);
+        List<Flight> flights = subject.Search(from, to, date);
         
-        Assert.Pass();
+        Assert.That(flights, Has.Count.EqualTo(1), "Only 1 flight expected");
+        Flight flight = flights[0];
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(flight.Id, Is.EqualTo(2), "Id not equal");
+            Assert.That(flight.From, Is.EqualTo(from), "From not equal");
+            Assert.That(flight.To, Is.EqualTo(to), "To not equal");
+            Assert.That(flight.Airline, Is.EqualTo("Oceanic Airlines"), "Airline not equal");
+            Assert.That(flight.Price, Is.EqualTo(245), "Price not equal");
+            Assert.That(flight.DepartureDate, Is.EqualTo(date), "DepartureDate not equal");
+        });
     }
 
 }
